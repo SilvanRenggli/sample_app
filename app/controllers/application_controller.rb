5f5f0831785ac9dac3a36b899a086b59efad5f2c
Silvan Renggli <sili.renggli@gmail.com>
2018-01-27 20:29:40 +0100

    Bold Hello World

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "<strong> hello world </strong>"
+    render html: "<p><strong> hello world </strong></p>"
   end
 end
